172bc2954c066d9623018b328a64356198ca57df
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-07-15 01:19:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution here!
+const drivers= [ 'Milo', 'Otis', 'Garfield' ]
+
+function destructivelyAppendDriver(){
+  drivers.push('Ralph')
+}
+
+function destructivelyPrependDriver(){
+  drivers.unshift('Bob')
+}
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift()
+}
+
+function appendDriver(){
+  let array= drivers;
+  array.push('Broom');
+  return array;
+}
